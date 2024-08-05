4afb467c47335b7649a8936eeb648c9a557a4040
sofiavakhnenko <sofia@vakhnenko.com>
2024-08-05 14:45:06 +0200

    Update

---
+++ BigHello.java
@@ -0,0 +1,9 @@
+public class BigHello {
+    public static void main(String[] args) {
+           System.out.println("*   * ***** *     *      *** ");
+           System.out.println("*   * *     *     *     *   *");
+           System.out.println("***** ***** *     *     *   *");
+           System.out.println("*   * *     *     *     *   *");
+           System.out.println("*   * ***** ***** *****  *** ");
+    }
+}
