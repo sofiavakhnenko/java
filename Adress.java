29bb83958f038658285396cc0678a0c0355b82ee
sofiavakhnenko <sofia@vakhnenko.com>
2024-08-05 14:35:51 +0200

    Update

---
+++ Adress.java
@@ -0,0 +1,7 @@
+public class Adress {
+    public static void main(String[] args) {
+           System.out.println("Berlinstrasse 1");
+           System.out.println("Berlin");
+           System.out.println("Deutschland");
+    }
+}
