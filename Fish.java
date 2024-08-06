26a9b1110c6adc94c1752f8b3b09b06f150d5cdb
sofiavakhnenko <sofia@vakhnenko.com>
2024-08-06 13:18:55 +0200

    Update

---
+++ Fish.java
@@ -0,0 +1,12 @@
+public class Fish extends Pet {
+    int currentDepth = 0;
+
+    public int dive(int howDeep) {
+        currentDepth = currentDepth + howDeep;
+        System.out.println("I drive deep "
+                + howDeep + " feet");
+        System.out.println("I am in the depths "
+                + currentDepth + " feet");
+        return currentDepth;
+    }
+}
---
+++ FishMaster.java
@@ -0,0 +1,8 @@
+public class FishMaster {
+    public static void main(String[] args) {
+        Fish myFish = new Fish();
+        myFish.dive(2);
+        myFish.dive(3);
+        myFish.sleep();
+    }
+}
