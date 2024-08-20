ab2a1cb600671c5724840ae0c5840ddc86fb02f8
sofiavakhnenko <sofia@vakhnenko.com>
2024-08-20 11:56:38 +0200

    Update

---
+++ ReportCard3.java
@@ -0,0 +1,40 @@
+public class ReportCard3 {
+    /**
+     * Die Methode "convertGrades" akzeptiert ein ganzzahliges Argument - das
+     * Testergebnis - und gibt
+     * abhaengig von diesem Argument das Zeichen A, B, C oder D zurueck
+     */
+    static char convertGrades(int testResult) {
+        char grade;
+        if (testResult >= 90) {
+            grade = 'A';
+        } else if (testResult >= 80 && testResult < 90) {
+            grade = 'B';
+        } else if (testResult >= 70 && testResult < 80) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+        return grade;
+    }
+
+    public static void main(String[] args) {
+        // ReportCard rc = new ReportCard();
+        char yourGrade = convertGrades(88);
+
+        switch (yourGrade) {
+            case 'A':
+                System.out.println("great!");
+                break;
+            case 'B':
+                System.out.println("good!");
+                break;
+            case 'C':
+                System.out.println("not bad!");
+                break;
+            case 'D':
+                System.out.println("try harder!");
+                break;
+        }
+    }
+}
